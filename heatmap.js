6955b98905e0a61e97c4c80b93df1b726800b9c6
Pieter Provoost <pieterprovoost@gmail.com>
2016-02-16 15:05:13 +0100

    multiple color scales

--- heatmap.js
+++ heatmap.js
@@ -28,7 +28,8 @@ angular.module("heatmap", []).directive("heatmap",
 					buckets: 9,
 					colors: ["#ffffd9", "#edf8b1", "#c7e9b4", "#7fcdbb", "#41b6c4", "#1d91c0", "#225ea8", "#253494", "#081d58"],
 					duration: 1000,
-					legendWidth: 0.3
+					legendWidth: 0.3,
+					breaks: null
 				};
 
 				if (scope.options) {
@@ -68,6 +69,11 @@ angular.module("heatmap", []).directive("heatmap",
 						yu[scope.data[i].y] = 0;
 					}
 
+					for (d in data) {
+						data[d].xIndex = x.indexOf(data[d].x);
+						data[d].yIndex = y.indexOf(data[d].y);
+					}
+
 					var xGridSize = Math.floor(width / x.length);
 					var yGridSize = Math.floor(height / y.length);
 					var legendElementWidth = Math.floor(width * options.legendWidth / (options.buckets));
@@ -93,9 +99,18 @@ angular.module("heatmap", []).directive("heatmap",
 						.attr("transform", "rotate(-90) translate(10, " + xGridSize / 2 + ")")
 						.attr("class", function(d, i) { return ("xLabel axis"); });
 
-					var colorScale = d3.scale.quantile()
-						.domain([0, options.buckets - 1, d3.max(scope.data, function (d) { return d.value; })])
-						.range(options.colors);
+					var colorScales = [];
+					if (options.breaks != null && options.breaks.length > 0) {
+						for (b in options.colors) {
+							colorScales.push(d3.scale.quantile()
+								.domain([0, options.buckets - 1, d3.max(scope.data, function (d) { return d.value; })])
+								.range(options.colors[b]));
+						}
+					} else {
+						colorScales.push(d3.scale.quantile()
+							.domain([0, options.buckets - 1, d3.max(scope.data, function (d) { return d.value; })])
+							.range(options.colors));
+					}
 
 					var cards = svg.selectAll(".square")
 						.data(scope.data);
@@ -103,17 +118,28 @@ angular.module("heatmap", []).directive("heatmap",
 					cards.append("title");
 
 					cards.enter().append("rect")
-						.attr("x", function(d) { return (x.indexOf(d.x)) * xGridSize; })
-						.attr("y", function(d) { return (y.indexOf(d.y)) * yGridSize; })
+						.attr("x", function(d) { return d.xIndex * xGridSize; })
+						.attr("y", function(d) { return d.yIndex * yGridSize; })
 						.attr("class", "square")
 						.attr("width", xGridSize)
 						.attr("height", yGridSize)
 						.on("click", function(d) { scope.dispatch.click(d); })
 						.on("mouseover", function(d) { scope.dispatch.mouseover(d); })
 						.on("mouseout", function(d) { scope.dispatch.mouseout(d); })
-						.style("fill", options.colors[0]);
+						.style("fill", "#ffffff");
 
-					cards.transition().duration(options.duration).style("fill", function(d) { return colorScale(d.value); });
+					cards.transition().duration(options.duration).style("fill", function(d) {
+						if (options.breaks != null && options.breaks.length > 0) {
+							for (b in options.breaks) {
+								if (d.xIndex < options.breaks[b]) {
+									return colorScales[b](d.value);
+								}
+							}
+							return colorScales[options.breaks.length](d.value);
+						} else {
+							return colorScales[0](d.value);
+						}
+					});
 
 					cards.select("title").text(function(d) { return d.value; });
 
@@ -122,7 +148,7 @@ angular.module("heatmap", []).directive("heatmap",
 					if (options.legend) {
 
 						var legend = svg.selectAll(".legend")
-							.data([0].concat(colorScale.quantiles()).concat(d3.max(scope.data, function (d) { return d.value; })), function(d) { return d; });
+							.data([0].concat(colorScales[0].quantiles()).concat(d3.max(scope.data, function (d) { return d.value; })), function(d) { return d; });
 
 						legend.enter().append("g").attr("class", "legend");
 
